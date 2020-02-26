cc9d3f8022363eb8d596dc89582efd2b916aa964
ikourski001 <ikourski@gmail.com>
2020-02-25 16:54:58 -0800

    added pulsar

---
+++ my-pulsar-python/consumer.py
@@ -0,0 +1,19 @@
+#https://pulsar.apache.org/docs/en/2.4.1/client-libraries-python/
+
+import pulsar
+
+client = pulsar.Client('pulsar://localhost:6650')
+
+consumer = client.subscribe('my-topic', 'my-subscription')
+
+while True:
+    msg = consumer.receive()
+    try:
+        print("Received message '{}' id='{}'".format(msg.data(), msg.message_id()))
+        # Acknowledge successful processing of the message
+        consumer.acknowledge(msg)
+    except:
+        # Message failed to be processed
+        consumer.negative_acknowledge(msg)
+
+client.close()
